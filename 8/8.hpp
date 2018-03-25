7ef138b0ac3eb9e4c0548921c20202c68e0022ec
LI, HONG-YU <zhenlinospirit@gmail.com>
2018-03-25 16:42:09 +0800

    In #8, remove std::function anyway.
    Summary: Although the runtime doesn't improve, remove <functional>
    anyway.

--- 8/8.hpp
+++ 8/8.hpp
@@ -5,7 +5,6 @@
 #include <cctype>   // isspace isdigit
 #include <climits>  // INT_MAX
 #include <cstring>
-#include <functional>
 #include <string>
 
 // Assumption: INT_MAX is 2147483647, and INT_MIN is -2147483648. This is given
@@ -18,33 +17,35 @@ class Solution {
   int myAtoi(std::string str) {
     auto const space = [](int c) { return std::isspace(c); };
     auto const signedCharacter = [](int c) { return c == '+' || c == '-'; };
-    auto const first = std::find_if_not(str.cbegin(), str.cend(), space);
+    auto first = std::find_if_not(str.cbegin(), str.cend(), space);
     if ((!str.empty() && first == str.cend()) ||
         (first != str.cend() && !signedCharacter(*first) &&
          !std::isdigit(*first))) {
       return 0;
     }
-
-    bool negative{};
-    int value{};
-    std::function<int(int, int)> op = std::plus<int>();
+    bool negative = false;
     if (*first == '+') {
       ++first;
     } else if (*first == '-') {
       negative = true;
-      op = std::minus<int>();
       ++first;
     }
-    for (auto it = first; it != str.cend() && std::isdigit(*it); ++it) {
-      if (!negative && (value > M || (value == M && ((*it) - '0') > 7)))
-        return INT_MAX;
-      else if (negative && (value < N || (value == N && ((*it) - '0') > 8)))
-        return INT_MIN;
-      value = op(10 * value, (*it) - '0');
+
+    int value{};
+    if (negative) {
+      for (auto it = first; it != str.cend() && std::isdigit(*it); ++it) {
+        if (value < N || (value == N && ((*it) - '0') > 8)) return INT_MIN;
+        value = 10 * value - ((*it) - '0');
+      }
+    } else {
+      for (auto it = first; it != str.cend() && std::isdigit(*it); ++it) {
+        if (value > M || (value == M && ((*it) - '0') > 7)) return INT_MAX;
+        value = 10 * value + ((*it) - '0');
+      }
     }
     return value;
   }
 
-  static constexpr int M = 214748364;
-  static constexpr int N = -214748364;
+  static constexpr int M = INT_MAX / 10;
+  static constexpr int N = INT_MIN / 10;
 };
