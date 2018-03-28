6a983521eb39e12a6ca7db3a27478eb3b66a6da6
LI, HONG-YU <zhenlinospirit@gmail.com>
2018-03-28 23:55:09 +0800

    Simplify #746.
    Summary: Make the implementation use less space. This is not optimized
    for runtime. The runtime of this version is, however, slightly worse
    than the previous one.

--- 746/sol.hpp
+++ 746/sol.hpp
@@ -8,49 +8,26 @@
 //     2. Climb 1 or 2 steps.
 
 #include <algorithm>
-#include <array>
-#include <cstddef>  // std::size_t
-#include <cstdint>  // std::int16_t
-#include <iostream>
-#include <iterator>
-#include <limits>
 #include <vector>
 
 class Solution {
-public:
-  // [2, 1000]
-  static constexpr std::size_t kMinCostLength = 2;
-  static constexpr std::size_t kMaxCostLength = 1000 + 1;
+ public:
+  using value_type = int;
+  using size_type = std::vector<value_type>::size_type;
 
-  // [0, 999]
-  static constexpr auto kMinCost = 0;
-  static constexpr auto kMaxCost = 999;
-
-  static_assert(std::numeric_limits<std::int16_t>::max() >= kMaxCost, "kMaxCost");
-
-public:
-  using Array = std::array<int, kMaxCostLength>;
-  using size_type = Array::size_type;
-
-public:
+ public:
   // Return the value of f(cost.size()).
   // Initial condition: f(0) = 0. f(1) = 0. (Both can be the starting point.)
   // f(1) = cost[0] + f(0) = cost[0]
   // f(2) = ...
   int minCostClimbingStairs(std::vector<int>& cost) {
-      Array f{};
-      f.fill(std::numeric_limits<int>::max());
-      f[0] = f[1] = size_type{};
-
-      // post-condition: f(i) is determined.
-      size_type i{};
-      for (; i < cost.size() - 1; ++i) {
-        f[i + 1] = std::min(f[i] + cost[i], f[i + 1]);
-        f[i + 2] = std::min(f[i] + cost[i], f[i + 2]);
-      }
-      if (i % 2) {
-        f[i + 1] = std::min(f[i] + cost[i], f[i + 1]);
-      }
-      return f[cost.size()];
+    value_type dp[] = {0, 0};  // f(0), f(1)
+    auto const size = cost.size();
+    size_type j = 0;
+    for (size_type i = 2; i <= size; ++i) {
+      dp[j] = std::min(dp[1 - j] + cost[i - 1], dp[j] + cost[i - 2]);
+      j = 1 - j;
     }
+    return dp[1 - j];
+  }
 };
